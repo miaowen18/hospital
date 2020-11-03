0877b990285d6ef635be0c49c019af6235c336d5
miaowen18 <zhxi94**>
2020-11-03 09:54:55 +0800

    我修改了配置文件数据库url

--- src/main/java/com/itgaoshu/hospital/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/itgaoshu/hospital/service/impl/SysUserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.itgaoshu.hospital.service.impl;
 
 import com.itgaoshu.hospital.bean.SysUser;
+import com.itgaoshu.hospital.mapper.SysUserMapper;
 import com.itgaoshu.hospital.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class SysUserServiceImpl implements SysUserService {
+
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
     @Override
     public SysUser selectByLoginname(SysUser user) {
-        return null;
+        return sysUserMapper.selectByLoginname(user);
     }
 }
