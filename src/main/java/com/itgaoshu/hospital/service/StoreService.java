c684e4de30325e62c8d99427da35e8e7b7b40089
miaowen18 <1371427548@qq.com>
2020-11-05 20:02:54 +0800

    kucun

--- src/main/java/com/itgaoshu/hospital/service/StoreService.java
+++ src/main/java/com/itgaoshu/hospital/service/StoreService.java
@@ -10,10 +10,14 @@ public interface StoreService {
     List<Type> queryList3();
     List<Unit> queryList4();
     List<Upplier> queryList5();
+    int selnumer(Drugstore drugstore);
+    int update(Drugstore drugstore);
+    int queryList1(Drugstore drugstore);
+    int addStore(Drugstore drugstore);
     List<Drugdictionary> selectdgty(Drugdictionary drugdictionary);
     List<Drugstore> seldrugDateguoqi();
     List<Drugstore> queryList(Drugstore drugstore);
-    List<DrugstoreVo> queryList(DrugstoreVo drugstoreVo);
+    //List<DrugstoreVo> queryList(DrugstoreVo drugstoreVo);
     List<Drugstore> queryQue();
     List<Caigou> queryList();
 }
